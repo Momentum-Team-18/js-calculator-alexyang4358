f71eab5884c01f3cadfc6e1a337694ab4806da50
alexyang4358 <alexyang4358@gmail.com>
2023-04-20 15:08:39 -0400

    v5 eval function added

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-// let featureDisplay = document.querySelectorAll (".current-num");
-// let button = document.querySelectorAll (".calculatorContainer")
-
-// for (let numbers of button){
-//     numbers.addEventListener("click",(event) =>{
-//         console.log(event.target.innerText);
-//         featureDisplay.innerText = event.target.innerText;
-//     });
-// }
-
-let calculatorContainer = document.querySelectorAll (".calculatorContainer");
-// let numberButton = document.querySelectorAll (".numberButton");
+let numberButton = document.querySelectorAll (".numberButton");
 let featureDisplay = document.querySelector ("#outputContainer");
+let calcButtons = document.querySelectorAll (".calcButton");
+let equals = document.querySelector ("#equals");
+let clears = document.querySelector ("#clear");
 
-for (let numbers of calculatorContainer){
-    numbers.addEventListener("click",(event) => {
+for (let buttons of calcButtons){
+    buttons.addEventListener("click",(event) => {
         console.log(event.target.innerText);
-        featureDisplay.innerText = event.target.innerText;
+        featureDisplay.innerText += event.target.innerText;
     });
 }
 
+equals.addEventListener("click", (event) => {
+    let answer = eval(featureDisplay.innerText);
+    console.log(answer); 
+    featureDisplay.innerText = answer;
+});
+
+clears.addEventListener("click", (event) => {
+    featureDisplay.innerText = '';
+});
